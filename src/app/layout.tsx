490622f489f98594f3c4f14668e1b0bfc9d79336
Quill <thatonequill@gmail.com>
2026-05-19 15:50:45 +0200

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,15 @@ export const metadata: Metadata = {
     //   { url: '/favicon-32x32.png', width: 32, height: 32, alt: 'QtQwill Favicon' },
     // ],
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
